df229c5ad8f0bf92f252f9c125ad101dd9a7af69
MPOstermann <mars15b@hotmail.com>
2019-04-04 00:10:57 -0700

    Updated ExcelWhisperRepository to log whisper messages in addition to the username. Added some basic formatting. Added a lock to prevent potential errors in case parallel access to the ExcelPackage causes issues.

--- src/API/TwitchShoppingNetworkLogger.Auditor/Impl/ExcelWhisperRepository.cs
+++ src/API/TwitchShoppingNetworkLogger.Auditor/Impl/ExcelWhisperRepository.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using TwitchShoppingNetworkLogger.Auditor.Interfaces;
 using TwitchShoppingNetworkLogger.Auditor.Models;
 
 namespace TwitchShoppingNetworkLogger.Auditor.Impl
 {
+    public class ExcelUserIndex
+    {
+        public string UserId { get; private set; }
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+
+        public ExcelUserIndex(string userId, int row)
+        {
+            UserId = userId;
+            Row = row;
+            Col = 2;
+        }
+
+        public void IncrementColumn()
+        {
+            Col++;
+        }
+    }
+
     public class ExcelWhisperRepository : IWhisperRepository
     {
         private const string WORKSHEET_NAME = "TSN_Logs";
 
+        // todo: need to store excel packages by session (or open a new package for each write; that might be inefficient though)
         private ExcelPackage _excelPackage;
+        private object _excelLock = new Object();
 
-        private IDictionary<string, IList<string>> _usersBySession;
+        private IDictionary<string, IList<ExcelUserIndex>> _usersBySession;
 
         public ExcelWhisperRepository()
         {
             _excelPackage = null;
-            _usersBySession = new Dictionary<string, IList<string>>();
+            _usersBySession = new Dictionary<string, IList<ExcelUserIndex>>();
         }
 
         public ISession CreateSessionForUser(string userId)
@@ -27,13 +51,28 @@ namespace TwitchShoppingNetworkLogger.Auditor.Impl
 
             // Create a new file
             var file = new FileInfo($"TSN_{userId}_{DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss")}.xlsx");
-            // todo: need to store excel packages by session (or open a new package for each write; that might be inefficient though)
-            _excelPackage = new ExcelPackage(file);
-            _excelPackage.Workbook.Worksheets.Add(WORKSHEET_NAME);
+            _excelPackage = CreateNewExcel(file); 
             _excelPackage.Save();
 
             // Initialize users list
-            _usersBySession.Add(retVal.Id, new List<string>());
+            _usersBySession.Add(retVal.Id, new List<ExcelUserIndex>());
+
+            return retVal;
+        }
+
+        private static ExcelPackage CreateNewExcel(FileInfo file)
+        {
+            var retVal = new ExcelPackage(file);
+            var sheet = retVal.Workbook.Worksheets.Add(WORKSHEET_NAME);
+
+            // Add headers and format the worksheet
+            sheet.Cells[1, 1].Value = "Users";
+            sheet.Cells[1, 2].Value = "Messages";
+            sheet.Row(1).Style.Font.Bold = true;
+            sheet.Row(1).Style.Fill.PatternType = ExcelFillStyle.Solid;
+            sheet.Row(1).Style.Fill.BackgroundColor.SetColor(Color.LightGreen);
+            sheet.Column(1).Style.Font.Bold = true;
+            sheet.Column(1).Width = 25;
 
             return retVal;
         }
@@ -50,21 +89,33 @@ namespace TwitchShoppingNetworkLogger.Auditor.Impl
 
         public void LogWhisper(IWhisperMessage whisper)
         {
-            int rowIndex = GetUserIndex(whisper.FromUserId, whisper.SessionId) + 1;
+            var index = GetUserIndex(whisper.FromUserId, whisper.SessionId);
             var sheet = _excelPackage.Workbook.Worksheets[WORKSHEET_NAME];
-            sheet.Cells[rowIndex, 1].Value = whisper.FromUsername;
-            _excelPackage.Save();
+
+            // Update the excel sheet with the whisper message
+            lock (_excelLock)
+            {
+                sheet.Cells[index.Row, 1].Value = whisper.FromUsername;
+                sheet.Cells[index.Row, index.Col].Value = whisper.Message;
+                _excelPackage.Save();
+            }
+
+            index.IncrementColumn();
         }
 
-        private int GetUserIndex(string userId, string sessionId)
+        private ExcelUserIndex GetUserIndex(string userId, string sessionId)
         {
             if (!_usersBySession.ContainsKey(sessionId))
                 throw new ArgumentException($"A session with id {sessionId} has not been started.");
 
             var users = _usersBySession[sessionId];
-            if (!users.Contains(userId))
-                users.Add(userId);
-            return users.IndexOf(userId);
+            if (!users.Any(n => n.UserId == userId))
+            {
+                var row = users.Count + 2;
+                users.Add(new ExcelUserIndex(userId, row));
+            }
+
+            return users.First(n => n.UserId == userId);
         }
 
         public bool HasUserWhisperedYet(string userId, string sessionId)
@@ -73,7 +124,7 @@ namespace TwitchShoppingNetworkLogger.Auditor.Impl
                 throw new ArgumentException($"A session with id {sessionId} has not been started.");
 
             var users = _usersBySession[sessionId];
-            return users.Contains(userId);
+            return users.Any(n => n.UserId == userId);
         }
     }
 }
