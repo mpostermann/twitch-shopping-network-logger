53b2cea2024204d4614db4e963166cf9381c33cc
MPOstermann <mars15b@hotmail.com>
2019-04-04 20:50:23 -0700

    Added console project to allow running program from the command line

---
+++ src/API/TwitchShoppingNetworkLogger.Console/Parameters.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TwitchShoppingNetworkLogger.Console
+{
+    public class Parameters
+    {
+        public string Username { get; private set; }
+        public string Token { get; private set; }
+
+        public Parameters(string[] args)
+        {
+            Username = args[0];
+            Token = args[1];
+        }
+    }
+}
---
+++ src/API/TwitchShoppingNetworkLogger.Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TwitchShoppingNetworkLogger.Auditor.Impl;
+using TwitchShoppingNetworkLogger.Auditor.Interfaces;
+using TwitchShoppingNetworkLogger.Config;
+
+namespace TwitchShoppingNetworkLogger.Console
+{
+    public class Program
+    {
+        private static IUserRepository _userRepository;
+        private static IAuditorRegistry _auditorRegistry;
+        private static IWhisperAuditor _auditor;
+
+        public static void Main(string[] args)
+        {
+            var parameters = GetParameters(args);
+
+            StartAuditing(parameters.Username, parameters.Token);
+            LoopApplication();
+            EndAuditing();
+        }
+
+        private static Parameters GetParameters(string[] args)
+        {
+            try
+            {
+                return new Parameters(args);
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("Usage: TwitchShoppingNetworkLogger.Console [username] [oauth token]");
+                Environment.Exit(0);
+                throw;
+            }
+        }
+
+        private static void StartAuditing(string username, string token)
+        {
+            _userRepository = new UserRepository(ConfigManager.Instance, null);
+            _auditorRegistry = new AuditorRegistry(_userRepository, ConfigManager.Instance);
+
+            _auditor = _auditorRegistry.RegisterNewWhisperAuditor(username, token, new ExcelWhisperRepository());
+            _auditor.StartAuditing();
+        }
+
+        private static void LoopApplication()
+        {
+            bool loop = true;
+            while (loop)
+            {
+                System.Console.WriteLine("Now logging whispers. Type 'quit' to end application.");
+                string command = System.Console.ReadLine() ?? string.Empty;
+                loop = !command.Trim().ToLower().Equals("quit");
+            }
+        }
+
+        private static void EndAuditing()
+        {
+            _auditor.EndAuditing();
+            System.Console.WriteLine("Client successfully disconnected.");
+        }
+    }
+}
